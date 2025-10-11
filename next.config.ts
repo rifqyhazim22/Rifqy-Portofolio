e389f38b9099e4a1f08d45874c5759f803d877f2
Rifqy Hazim H R <rifqyhazim22@gmail.com>
2025-10-11 17:28:34 +0700

    Raise global API body size limit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, "./"),
+  api: {
+    bodyParser: {
+      sizeLimit: "12mb",
+    },
+  },
 };
 
 export default nextConfig;
